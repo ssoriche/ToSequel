656fb0e5a590b0980db7625189d99dc15ebc42cc
Shawn Sorichetti <ssoriche@gmail.com>
2012-09-05 14:48:02 -0400

    Create a base class for Commands
    Move shared attributes into the new base class.

---
+++ lib/App/ToSequel/Command.pm
@@ -0,0 +1,82 @@
+package App::ToSequel::Command;
+use App::Cmd::Setup -command;
+
+#ABSTRACT: Base class for App::ToSequel commands
+use strict;
+use warnings;
+
+use Text::xSV;
+
+=attr tablename
+
+=cut
+sub tablename {
+  my ($self, $tablename) = @_;
+  if ($tablename) {
+    $self->{tablename} = $tablename;
+  }
+
+  return $self->{tablename};
+}
+
+=attr schema
+
+=cut
+sub schema {
+  my ($self, $schema) = @_;
+  if ($schema) {
+    $self->{schema} = $schema;
+  }
+
+  return $self->{schema};
+}
+
+=attr columns
+  [
+    { name
+      position
+      maxlength
+      datatype
+    }
+  ]
+=cut
+
+sub columns {
+  my ($self, $args) = @_;
+
+  $self->{columns} = $args if($args);
+
+  return $self->{columns};
+}
+
+=attr csv($filename)
+
+Create a Text::xSV object from the filename.
+
+=cut
+sub csv {
+  my ($self, $filename) = @_;
+  if ($filename) {
+    $self->{csv} = Text::xSV->new;
+    $self->{csv}->open_file($filename);
+  }
+
+  return $self->{csv};
+}
+
+=method extract_columns
+
+Using the header from the CSV file create column entries
+
+=cut
+sub extract_columns {
+  my ($self, $args) = @_;
+
+  my $columns = $self->csv->read_header;
+
+  my @columnlist = map {name => $_, position => $columns->{$_}}, sort { $columns->{$a} <=> $columns->{$b} } keys(%$columns);
+  $self->columns(\@columnlist);
+}
+
+
+1;
--- lib/App/ToSequel/Command/ddl.pm
+++ lib/App/ToSequel/Command/ddl.pm
@@ -5,59 +5,12 @@ use App::ToSequel -command;
 use strict;
 use warnings;
 
-use Text::xSV;
-
 sub usage_desc { "tosequel %o [csvfile]" }
 
 sub opt_spec {
   return (
     [ "tablename=s",  "tablename to create", ],
   );
-}
-
-=attr tablename
-
-=cut
-sub tablename {
-  my ($self, $tablename) = @_;
-  if ($tablename) {
-    $self->{tablename} = $tablename;
-  }
-
-  return $self->{tablename};
-}
-
-=attr columns
-  [
-    { name
-      position
-      maxlength
-      datatype
-    }
-  ]
-=cut
-
-sub columns {
-  my ($self, $args) = @_;
-
-  $self->{columns} = $args if($args);
-
-  return $self->{columns};
-}
-
-=attr csv($filename)
-
-Create a Text::xSV object from the filename.
-
-=cut
-sub csv {
-  my ($self, $filename) = @_;
-  if ($filename) {
-    $self->{csv} = Text::xSV->new;
-    $self->{csv}->open_file($filename);
-  }
-
-  return $self->{csv};
 }
 
 sub execute {
@@ -69,20 +22,6 @@ sub execute {
   $self->extract_columns;
   $self->column_lengths;
   print $self->ddl;
-}
-
-=method extract_columns
-
-Using the header from the CSV file create column entries
-
-=cut
-sub extract_columns {
-  my ($self, $args) = @_;
-
-  my $columns = $self->csv->read_header;
-
-  my @columnlist = map {name => $_, position => $columns->{$_}}, sort { $columns->{$a} <=> $columns->{$b} } keys(%$columns);
-  $self->columns(\@columnlist);
 }
 
 sub column_lengths {
