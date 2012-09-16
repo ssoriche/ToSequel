9ac7554cabef52bdb75ed87294a4bd0f2499a091
Shawn Sorichetti <ssoriche@gmail.com>
2012-09-16 10:59:35 -0400

    Create varchar method for Oracle.
    Because you know, varchar can't just be varchar in Oracle, has to be
    varchar2...

--- lib/App/ToSequel/DB/Oracle.pm
+++ lib/App/ToSequel/DB/Oracle.pm
@@ -1,4 +1,25 @@
+package App::ToSequel::DB::Oracle;
+
+#ABSTRACT: Database engine interface plugin for Oracle
 
 sub command_option {
-  return [ "Oracle|O",  "Oracle Database", ],
+  return [ "Oracle|O",  "Oracle Database", ];
 }
+
+=attr varchar
+
+=cut
+sub varchar {
+  my ($self,$args) = @_;
+
+  return 'VARCHAR2('. $args->{length} .')';
+}
+
+sub new {
+  my $class = shift;
+  $class = ref $class if ref $class;
+  my $self = bless {}, $class;
+  $self;
+}
+
+1;
